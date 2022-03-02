df097225c69efaa3eebd816ba0737bf9b7d6f76b
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-02 12:10:56 +0700

    Register activation hook

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -12,3 +12,22 @@ Author URI:     https://study-hary-id.github.io
 License:        GPLv2 or later
 Text Domain:    alecadd-plugin
 */
+
+if (!defined('ABSPATH')) {
+    die('-1');
+}
+
+class AlecaddPlugin {
+    function activate() {}
+
+    function deactivate() {}
+
+    // function uninstall() {}
+}
+
+if (class_exists('AlecaddPlugin')) {
+    $alecaddPlugin = new AlecaddPlugin;
+}
+
+register_activation_hook(__FILE__, array($alecaddPlugin, 'activate'));
+register_deactivation_hook(__FILE__, array($alecaddPlugin, 'deactivate'));
