8e2c26e723c5b0434a338a7e4482f0c53ee3b388
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-02 11:36:36 +0700

    Initial commit

---
+++ alecadd-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package AlecaddPlugin
+ */
+/*
+Plugin Name:    Alecadd Plugin
+Plugin URI:     https://study-hary-id.github.io
+Description:    This is my first attempt on writing a custom plugin for this amazing tutorial series.
+Version:        1.0.0
+Author:         Muhammad Haryansyah
+Author URI:     https://study-hary-id.github.io
+License:        GPLv2 or later
+Text Domain:    alecadd-plugin
+*/
