ccaee827b061e5e9eff365ca93cada48579c74d2
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 11:56:48 +0700

    Refactor alecadd-plugin.php

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -17,39 +17,41 @@ if (!defined('ABSPATH')) {
     die('-1');
 }
 
-class AlecaddPlugin {
-    function __construct() {
-        $this->create_post_type();
+if (!class_exists('AlecaddPlugin')) {
+    class AlecaddPlugin {
+        function __construct() {
+            $this->create_post_type();
+        }
+
+        function register() {
+            add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+            add_action('admin_menu', array($this, 'add_admin_pages'));
+        }
+
+        function enqueue() {
+            wp_enqueue_style('pluginstyle', plugins_url('/assets/css/style.css', __FILE__));
+            wp_enqueue_script('pluginstyle', plugins_url('/assets/js/script.js', __FILE__));
+        }
+
+
+        function custom_post_type() {
+            register_post_type('book', ['public' => true, 'label' => 'Books']);
+        }
+
+        protected function create_post_type() {
+            add_action('init', array($this, 'custom_post_type'));
+        }        
     }
 
-    function register() {
-        add_action('admin_enqueue_scripts', array($this, 'enqueue'));
-    }
-
-    function custom_post_type() {
-        register_post_type('book', ['public' => true, 'label' => 'Books']);
-    }
-
-    function create_post_type() {
-        add_action('init', array($this, 'custom_post_type'));
-    }
-
-    function enqueue() {
-        wp_enqueue_style('pluginstyle', plugins_url('/assets/css/style.css', __FILE__));
-        wp_enqueue_script('pluginstyle', plugins_url('/assets/js/script.js', __FILE__));
-    }
-}
-
-if (class_exists('AlecaddPlugin')) {
     $alecaddPlugin = new AlecaddPlugin;
     $alecaddPlugin->register();
+
+
+    // Activate the plugin.
+    require_once plugin_dir_path(__FILE__) . 'inc/alecadd-plugin-activate.php';
+    register_activation_hook(__FILE__, array('AlecaddPluginActivate', 'activate'));
+
+    // Deactivate the plugin.
+    require_once plugin_dir_path(__FILE__) . 'inc/alecadd-plugin-deactivate.php';
+    register_deactivation_hook(__FILE__, array('AlecaddPluginDeactivate', 'deactivate'));
 }
-
-
-// Activate the plugin.
-require_once plugin_dir_path(__FILE__) . 'inc/alecadd-plugin-activate.php';
-register_activation_hook(__FILE__, array('AlecaddPluginActivate', 'activate'));
-
-// Deactivate the plugin.
-require_once plugin_dir_path(__FILE__) . 'inc/alecadd-plugin-deactivate.php';
-register_deactivation_hook(__FILE__, array('AlecaddPluginDeactivate', 'deactivate'));
