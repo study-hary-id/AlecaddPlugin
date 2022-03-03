77ac933fa4551b72f746f005d60b926423507411
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 16:51:15 +0700

    Register Activation and Deactivation hooks

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -19,8 +19,22 @@ if (!defined('ABSPATH')) {
 
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN', plugin_basename(__FILE__));
 
 require_once PLUGIN_PATH . 'inc/Init.php';
+require_once PLUGIN_PATH . 'inc/Base/Activation.php';
+require_once PLUGIN_PATH . 'inc/Base/Deactivation.php';
+
+function activate_alecadd_plugin() {
+    Activation::activate();
+}
+
+function deactivate_alecadd_plugin() {
+    Deactivation::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_alecadd_plugin');
+register_deactivation_hook(__FILE__, 'deactivate_alecadd_plugin');
 
 if (class_exists('Init')) {
     Init::register_services();
