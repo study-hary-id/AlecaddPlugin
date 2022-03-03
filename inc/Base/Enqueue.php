9d96c41c8d22aac0937327a739f9f535c94025f3
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 16:46:06 +0700

    Add public access modifier

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -8,7 +8,7 @@ class Enqueue {
         add_action('admin_enqueue_scripts', array($this, 'enqueue_plugin'));
     }
 
-    function enqueue_plugin() {
+    public function enqueue_plugin() {
         wp_enqueue_style('pluginstyle', PLUGIN_URL . '/assets/css/style.css');
         wp_enqueue_script('pluginstyle', PLUGIN_URL . '/assets/js/script.js');
     }
