929c649a74147d5f6e4723ff775b5e1b5302c2ee
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 09:45:00 +0700

    Add custom post type named Books

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -18,11 +18,24 @@ if (!defined('ABSPATH')) {
 }
 
 class AlecaddPlugin {
-    function activate() {}
+    function __construct() {
+        add_action('init', array($this, 'custom_post_type'));
+    }
 
-    function deactivate() {}
+    function activate() {
+        $this->custom_post_type();
+        flush_rewrite_rules();
+    }
+
+    function deactivate() {
+        flush_rewrite_rules();
+    }
 
     // function uninstall() {}
+
+    function custom_post_type() {
+        register_post_type('book', ['public' => true, 'label' => 'Books']);
+    }
 }
 
 if (class_exists('AlecaddPlugin')) {
