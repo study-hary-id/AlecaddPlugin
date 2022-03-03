83501a5187a887fb001acf3a987eaf4f352c0f0c
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 15:29:46 +0700

    Add Enqueue class

---
+++ inc/Base/Enqueue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package AlecaddPluginCustom
+ */
+
+class Enqueue {
+    public function register() {
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_plugin'));
+    }
+
+    function enqueue_plugin() {
+        wp_enqueue_style('pluginstyle', PLUGIN_URL . '/assets/css/style.css');
+        wp_enqueue_script('pluginstyle', PLUGIN_URL . '/assets/js/script.js');
+    }
+}
