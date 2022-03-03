9e2e2370f1b707f4b78b5c3a9381131dbd78ade8
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-03 10:16:23 +0700

    Add uninstall.php to do custom uninstallation

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package AlecaddPlugin
+ */
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die('-1');
+}
+
+$books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
+
+foreach($books as $book) {
+    wp_delete_post($book->ID, false);
+}
