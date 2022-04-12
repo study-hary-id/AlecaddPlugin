ef3f476fb231893fb30cf95cb78ff5dc5867ebbb
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-12 20:56:55 +0700

    Refactor and update SimplePlugin

--- simple-plugin.php
+++ simple-plugin.php
@@ -33,105 +33,157 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
 
-if (!defined('ABSPATH')) {
+if ( !defined( 'ABSPATH' ) ) {
     die('-1');
 }
 
-if (!class_exists('AlecaddPlugin')) {
-    class AlecaddPlugin {
-        public $plugin;
+if ( !class_exists( 'SimplePlugin' ) ) {
+    /**
+     * Class SimplePlugin is a constructor for this plugin.
+     */
+    class SimplePlugin
+    {
+        private $plugin;
 
-        function __construct() {
-            $this->plugin = plugin_basename(__FILE__);
+        function __construct()
+        {
+            $this->plugin = plugin_basename( __FILE__ );
             $this->create_post_type();
         }
 
         /**
-         * Add/register new action or filter to each hooks.
-         * 
-         * @return void
+         * Listener uses when activate the plugin.
          */
-        function register() {
-            add_action('admin_enqueue_scripts', array($this, 'enqueue'));
-
-            add_action('admin_menu', array($this, 'add_admin_pages'));
-
-            add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+        function activate()
+        {
+            flush_rewrite_rules();
         }
 
         /**
-         * Enqueue stylesheet and javascript files.
-         * 
+         * Listener uses when deactivate the plugin.
+         */
+        function deactivate()
+        {
+            flush_rewrite_rules();
+        }
+
+
+        /**
+         * Add/register new action or filter to each hooks.
+         *
          * @return void
          */
-        function enqueue() {
-            wp_enqueue_style('pluginstyle', plugins_url('/assets/css/style.css', __FILE__));
-            wp_enqueue_script('pluginstyle', plugins_url('/assets/js/script.js', __FILE__));
+        function register()
+        {
+            add_filter(
+                "plugin_action_links_$this->plugin",
+                array( $this, 'settings_link' )
+            );
+
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+
+            add_action( 'admin_menu', array( $this, 'add_admin_pages' ));
+        }
+
+
+        /**
+         * Add redirect link to the plugin's settings.
+         *
+         * @param array $links  List of necessary links.
+         * @return array        Return list after added a new link element.
+         */
+        function settings_link($links)
+        {
+            $settings_link = '<a href="admin.php?page=simple_plugin">Settings</a>';
+            array_push( $links, $settings_link );
+            return $links;
+        }
+
+
+        /**
+         * Enqueue stylesheet and javascript files.
+         *
+         * @return void
+         */
+        function enqueue()
+        {
+            wp_enqueue_style(
+                'simpleplugin',
+                plugins_url( '/assets/css/style.css', __FILE__ )
+            );
+            wp_enqueue_script(
+                'simpleplugin',
+                plugins_url( '/assets/js/script.js', __FILE__ )
+            );
+        }
+
+
+        /**
+         * Import the template of custom admin pages.
+         *
+         * @return void
+         */
+        function admin_views()
+        {
+            require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
         }
 
         /**
          * Add/register new menu on admin side-bar.
-         * 
+         *
          * @return void
          */
-        function add_admin_pages() {
+        function add_admin_pages()
+        {
             add_menu_page(
-                'Alecadd Plugin',
-                'Alecadd',
+                'Simple Plugin',
+                'Plugin',
                 'manage_options',
-                'alecadd_plugin',
-                array($this, 'admin_index'),
+                'simple_plugin',
+                array( $this, 'admin_views' ),
                 'dashicons-store',
                 110
             );
         }
 
-        /**
-         * Add custom link onto plugin list.
-         * 
-         * @param array $links  List of necessary links.
-         * @return array        Return list after added a new link element.
-         */
-        function settings_link($links) {
-            $settings_link = '<a href="admin.php?page=alecadd_plugin">Settings</a>';
-            array_push($links, $settings_link);
-            return $links;
-        }
-
-
-        function admin_index() {
-            require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
-        }
-
 
         /**
          * Register new custom post type.
-         * 
+         *
+         * This function behaves as a dependency before init create custom post type.
+         *
          * @return void
          */
-        function custom_post_type() {
-            register_post_type('book', ['public' => true, 'label' => 'Books']);
+        function custom_post_type()
+        {
+            register_post_type(
+                'book',
+                [ 'public' => true, 'label' => 'Books' ]
+            );
         }
 
         /**
          * Initialize custom post type by calling init hook.
-         * 
+         *
          * @return void
          */
-        protected function create_post_type() {
-            add_action('init', array($this, 'custom_post_type'));
+        function create_post_type()
+        {
+            add_action( 'init', array( $this, 'custom_post_type' ) );
         }
     }
 
-    $alecaddPlugin = new AlecaddPlugin;
-    $alecaddPlugin->register();
+    // Create an instance and register hooks.
+    $simple_plugin = new SimplePlugin();
+    $simple_plugin->register();
 
+    // Register listener for activation of the plugin.
+    register_activation_hook(
+        __FILE__, array( $simple_plugin, 'activate' )
+    );
 
-    // Register activation the plugin.
-    require_once plugin_dir_path(__FILE__) . 'inc/Base/Activate.php';
-    register_activation_hook(__FILE__, array('AlecaddPluginActivate', 'activate'));
-
-    // Register deactivation the plugin.
-    require_once plugin_dir_path(__FILE__) . 'inc/Base/Deactivate.php';
-    register_deactivation_hook(__FILE__, array('AlecaddPluginDeactivate', 'deactivate'));
+    // Register listener for deactivation of the plugin.
+    register_deactivation_hook(
+        __FILE__, array( $simple_plugin, 'deactivate' )
+    );
 }
