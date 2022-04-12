cce8154f053b66fa6fd881c793b62b3ee50627d1
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-12 19:20:51 +0700

    Add internal license

--- simple-plugin.php
+++ simple-plugin.php
@@ -16,14 +16,21 @@
  * Text Domain:       simple-plugin
  */
 /*
-Plugin Name:    Alecadd Plugin
-Plugin URI:     https://study-hary-id.github.io
-Description:    This is my first attempt on writing a custom plugin for this amazing tutorial series.
-Version:        1.0.0
-Author:         Muhammad Haryansyah
-Author URI:     https://study-hary-id.github.io
-License:        GPLv2 or later
-Text Domain:    alecadd-plugin
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
 */
 
 if (!defined('ABSPATH')) {
